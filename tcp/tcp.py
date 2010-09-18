4621bc8e54fa5cda122d491f6c161439bb03d06c
sigh <sigh.projects@gmail.com>
2010-09-18 21:09:44 +1000

    Added once option and changed name options

--- tcp/tcp.py
+++ tcp/tcp.py
@@ -8,23 +8,28 @@ import getopt
 OPTIONS = {
     'ip': '213.3.30.106',
     'port': '9999',
-    'base_name': 'sigh',
-    'name': '',
+    'name': 'sigh',
+    'version': '',
     'bot_file': './src/MyBot'
 }
 
 def run_game():
     p = subprocess.Popen( 
-        ['tcp/tcp', OPTIONS['ip'], OPTIONS['port'], OPTIONS['base_name']+OPTIONS['name'], OPTIONS['bot_file']],
-        stdout=sys.stdout, 
-        stderr=sys.stderr
+        ['tcp/tcp', OPTIONS['ip'], OPTIONS['port'], OPTIONS['name']+'-'+OPTIONS['version'], OPTIONS['bot_file']],
+        stdout=sys.stderr,
+        stderr=sys.stdout
+        # stdout=subprocess.PIPE, 
+        # stderr=subprocess.PIPE
     )
+    # (stdout, stderr) = p.communicate()
     p.wait()
+    # print stdout
 
 def parse_args():
-    opts, args = getopt.getopt(sys.argv[1:], "", [o+"=" for o in OPTIONS])
+    opts, args = getopt.getopt(sys.argv[1:], "", [o+"=" for o in OPTIONS] + ['once'])
     for o, a in opts:
         OPTIONS[o[2:]] = a
+    OPTIONS['once'] = ( OPTIONS.get('once') != None )
 
 stop = False
 def stop_handler(*args):
@@ -36,6 +41,7 @@ if __name__ == "__main__":
 
     signal.signal(signal.SIGINT, stop_handler)
 
-    #run_game()
     while not stop:
         run_game()
+        if OPTIONS['once']:
+            stop_handler()
