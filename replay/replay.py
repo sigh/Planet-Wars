f66274266361bdb820b3fee110fd2c32d27cd146
sigh <sigh.projects@gmail.com>
2010-09-19 12:26:34 +1000

    Added options to replay

--- replay/replay.py
+++ replay/replay.py
@@ -3,8 +3,14 @@
 import sys
 from urllib import urlopen
 import re
+import getopt
 
-URL_BASE="http://www.ai-contest.com/visualizer.php?game_id="
+OPTIONS = {
+    'url': "http://www.ai-contest.com/visualizer.php",
+    'param': 'game_id',
+    'fleet_file': 'fleet_commands.txt',
+    'player': 1
+}
 
 def parse_planet(input):
     input = input.split(',')
@@ -17,30 +23,29 @@ def parse_planet(input):
         'growth': input[4]
     }
 
-def output_map(planets):
-    for p in planets:
-        print "P %s %s %s %s %s" %(p['x'], p['y'], p['owner'], p['ships'], p['growth'])
+def map_data(playback_string):
+    playback = playback_string.split('|')
+    return '\n'.join(
+        "P %s %s %s %s %s" %(p['x'], p['y'], p['owner'], p['ships'], p['growth'])
+        for p in map(parse_planet, playback[0].split(':'))
+    )
 
 def get_data(game_id):
-    page = urlopen(URL_BASE + str(game_id)).read()
+    page = urlopen(OPTIONS['url'] + '?' + OPTIONS['param'] + '=' + str(game_id)).read()
     match = re.search(r'var data = "([^"]+)"', page)
-    if not match:
-        return
-        
-    data = {}
-    for item in match.group(1).split('\\n'):
-        parts = item.split('=',2)
-        if len(parts) == 2:
-            data[parts[0]] = parts[1]
+    if match:
+        return dict(item.split('=',2) for item in match.group(1).split('\\n') if item)
 
-    return data
+def parse_args():
+    opts, args = getopt.getopt(sys.argv[1:], "", [o+"=" for o in OPTIONS])
+    for o, a in opts:
+        OPTIONS[o[2:]] = a
+    return args
 
 if __name__ == "__main__":
-    data = get_data(sys.argv[1])
+    args = parse_args()
+    data = get_data(args[0])
 
-    playback_string = data['playback_string']
-    playback = playback_string.split('|')
+    map = map_data(data['playback_string'])
 
-    planets = map(parse_planet, playback[0].split(':'))
-
-    output_map(planets)
+    print map
