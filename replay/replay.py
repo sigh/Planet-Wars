266a718d7129afe7cfdd0a784f05e5c065c963fd
sigh <sigh.projects@gmail.com>
2010-09-19 12:56:13 +1000

    Add fleet parsing

--- replay/replay.py
+++ replay/replay.py
@@ -23,12 +23,38 @@ def parse_planet(input):
         'growth': input[4]
     }
 
+def parse_fleet(input):
+    input = input.split('.')
+    # (x,y,owner,numShips,growthRate)
+    return {
+        'owner': input[0],
+        'ships': input[1],
+        'source': input[2],
+        'destination': input[3],
+        'trip_length': input[4],
+        'progress': int(input[4]) - int(input[5])
+    }
+
 def map_data(playback_string):
-    playback = playback_string.split('|')
     return '\n'.join(
         "P %s %s %s %s %s" %(p['x'], p['y'], p['owner'], p['ships'], p['growth'])
-        for p in map(parse_planet, playback[0].split(':'))
+        for p in get_planets(playback_string)
     )
+
+def get_planets(playback_string):
+    return map(parse_planet, playback_string.split('|')[0].split(':'))
+
+def fleet_data(playback_string):
+    playback = playback_string.split('|')
+    if len(playback) < 2:
+        return []
+    num_planets = len(get_planets(playback_string))
+
+    all_fleets = []
+    for turn in playback[1].split(':'):
+        all_fleets.append( map(parse_fleet, turn.split(',')[num_planets:]) )
+
+    return all_fleets
 
 def get_data(game_id):
     page = urlopen(OPTIONS['url'] + '?' + OPTIONS['param'] + '=' + str(game_id)).read()
@@ -46,6 +72,7 @@ if __name__ == "__main__":
     args = parse_args()
     data = get_data(args[0])
 
-    map = map_data(data['playback_string'])
+    map_string = map_data(data['playback_string'])
+    fleets = fleet_data(data['playback_string'])
 
-    print map
+    # print map_string
