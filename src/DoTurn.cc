01da2b6132032418e0f743c17d70d79adcaa8f68
sigh <sigh.projects@gmail.com>
2010-10-07 21:50:11 +1100

    Attack without scores!

--- src/DoTurn.cc
+++ src/DoTurn.cc
@@ -15,6 +15,10 @@ typedef std::map<int, std::pair<int,int> > DefenceExclusions;
 void Defence(GameState& state);
 DefenceExclusions AntiRage(GameState& state);
 int AntiRageRequiredShips(const GameState &state, const PlanetPtr& my_planet, const PlanetPtr& enemy_planet);
+void Attack(GameState& state, DefenceExclusions& defence_exclusions);
+
+int ScorePlanet(const GameState& state, PlanetPtr p, const DefenceExclusions& defence_exclusions);
+int ScorePlanet(const GameState& state, PlanetPtr p, const DefenceExclusions& defence_exclusions, std::vector<Fleet>& orders);
 
 void DoTurn(const GameState& initial_state, std::vector<Fleet>& orders) {
     // Create a mutable version of the state
@@ -23,6 +27,8 @@ void DoTurn(const GameState& initial_state, std::vector<Fleet>& orders) {
     Defence(state);
 
     DefenceExclusions defence_exclusions = AntiRage(state);
+
+    Attack(state, defence_exclusions);
 
     // Populate orders array with the orders that were generated
     orders = state.Orders();
@@ -142,14 +148,123 @@ int AntiRageRequiredShips(const GameState &state, const PlanetPtr& my_planet, co
 }
 
 /*
+ * Attack
+ */
+
+void Attack(GameState& state, DefenceExclusions& defence_exclusions) {
+    static bool attack = Config::Value<bool>("attack");
+    if ( ! attack  ) return;
+
+    LOG("Attack phase");
+
+    std::vector<PlanetPtr> planets = state.Planets();
+    std::vector< std::pair<int,int> > scores;
+
+    std::map<int,bool> targets_1 = state.FrontierPlanets(ENEMY); 
+    std::map<int,bool> targets_2 = state.FutureFrontierPlanets(ENEMY); 
+    for (unsigned int p_id=0; p_id<planets.size(); ++p_id) {
+        const PlanetPtr p = planets[p_id];
+        int growth_rate = Map::GrowthRate(p_id);
+
+        // Ignore 0 growth planets for now
+        // LATER: put it back in FOR ENEMIES ONLY
+        // Beware of divide by 0
+        if ( growth_rate == 0 ) {
+            continue;
+        }
+
+        // Don't need to do anything if we will own the planet
+        // TODO: Handle in the Distribution phase
+        int future_owner = p->FutureOwner();
+        if ( future_owner == ME) {
+            continue;
+        }
+
+        // If a neutral planet is closer to an enemy then ignore it
+        if ( future_owner == NEUTRAL ) {
+
+            PlanetPtr closest_enemy = state.ClosestPlanetByOwner( p, ENEMY );
+            PlanetPtr closest_me = state.ClosestPlanetByOwner( p, ME );
+
+            if ( closest_enemy && closest_me && Map::Distance( closest_enemy->id, p_id ) <= Map::Distance( closest_me->id, p_id) ) {
+                continue;
+            }
+        }
+
+        // If the planet is owned by an enemy and it is NOT a frontier planet then ignore
+        if ( p->Owner() == ENEMY && ! ( targets_1[p_id] || targets_2[p_id] ) ) {
+            continue;
+        }
+
+        // Don't try to neutral steal planets that we would not otherwise attack
+        if ( future_owner == ENEMY && p->Owner() == NEUTRAL && ! targets_2[p_id] ) {
+            continue;
+        }
+
+        int score = ScorePlanet(state, p, defence_exclusions);
+        scores.push_back( std::pair<int,int>(score, p_id) );
+    }
+
+    // sort scores
+    std::sort(scores.begin(), scores.end());
+
+    LOG(" Starting attacks");
+
+    // start attacking planets based on score
+    std::pair<int,int> s;
+    foreach ( s, scores ) {
+        PlanetPtr p = state.Planet(s.second);
+
+        // list of the orders that we will want to execute
+        std::vector<Fleet> orders;
+
+        ScorePlanet(state, p, defence_exclusions, orders);
+
+        if ( orders.empty() ) {
+            // This case happens when cost > available_ships
+
+            // If we don't have enough ships to capture the next best planet then WAIT
+            //  This serves a few purposes:
+            //  1. Prevent us overextending our forces
+            //  2. If we wait we might get the required resources later
+            //  3. This helps against Rage tactics
+            // Harass(state, p->id, orders);
+            break;
+        }
+
+        // update delays so all fleet arrive at once
+        // TODO: Try leaving this out (Might harm neutral attack/overtakes)
+        const Fleet& last_order = orders.back();
+        int last_arrival = last_order.launch + Map::Distance( last_order.source, last_order.dest );
+        foreach ( Fleet& order, orders ) {
+            order.launch = last_arrival - Map::Distance( order.source, order.dest );
+        }
+
+        // If we reached here we want to actually execute the orders
+        foreach ( Fleet& order, orders ) {
+            state.IssueOrder(order);
+        }
+        
+    }
+}
+
+int ScorePlanet(const GameState& state, PlanetPtr p, const DefenceExclusions& defence_exclusions) {
+    std::vector<Fleet> orders;
+    return ScorePlanet(state, p, defence_exclusions, orders);
+}
+
+// Does a cost analysis for taking over planet p and populates orders with the orders required
+// to do so.
+// Returns score
+int ScorePlanet(const GameState& state, PlanetPtr p, const DefenceExclusions& defence_exclusions, std::vector<Fleet>& orders) {
+    return 0;
+}
+/*
  
 
 void Flee(PlanetWars& pw);
-void Attack(PlanetWars& pw, DefenceExclusions& defence_exclusions);
 void Redistribution(PlanetWars& pw);
 void Harass(PlanetWars& pw, int planet, std::vector<Fleet>& orders);
-int ScorePlanet(const PlanetWars& pw, PlanetPtr p, const DefenceExclusions& defence_exclusions);
-int ScorePlanet(const PlanetWars& pw, PlanetPtr p, const DefenceExclusions& defence_exclusions, std::vector<Fleet>& orders);
 
 int ScoreEdge(const PlanetWars& pw, PlanetPtr dest, PlanetPtr source, int available_ships, int source_ships, int delay, int& cost, std::vector<Fleet>& orders);
 
@@ -173,104 +288,6 @@ void DoTurn(PlanetWars& pw, int turn) {
     LOG("Finishing up");
 }
 
-void Attack(PlanetWars& pw, DefenceExclusions& defence_exclusions) {
-    static bool attack = Config::Value<bool>("attack");
-    if ( ! attack  ) return;
-
-    LOG("Attack phase");
-
-    std::vector<PlanetPtr> planets = pw.Planets();
-    std::vector< std::pair<int,int> > scores;
-
-    std::map<int,bool> targets_1 = FrontierPlanets(pw, ENEMY); 
-    std::map<int,bool> targets_2 = FutureFrontierPlanets(pw, ENEMY); 
-    for (int p_id=0; p_id<planets.size(); ++p_id) {
-        const PlanetPtr p = planets[p_id];
-        int growth_rate = Map::GrowthRate(p_id);
-
-        // Ignore 0 growth planets for now
-        // LATER: put it back in FOR ENEMIES ONLY
-        // Beware of divide by 0
-        if ( growth_rate == 0 ) {
-            continue;
-        }
-
-        // Don't need to do anything if we will own the planet
-        // TODO: Handle in the Distribution phase
-        int future_owner = p->FutureOwner();
-        if ( future_owner == ME) {
-            continue;
-        }
-
-        // If a neutral planet is closer to an enemy then ignore it
-        if ( future_owner == NEUTRAL ) {
-
-            int closest_enemy = ClosestPlanetByOwner( pw, p_id, ENEMY );
-            int closest_me = ClosestPlanetByOwner( pw, p_id, ME );
-
-            if ( closest_enemy >= 0 && closest_me >= 0  && Map::Distance( closest_enemy, p_id ) <= Map::Distance( closest_me, p_id) ) {
-                continue;
-            }
-        }
-
-        // If the planet is owned by an enemy and it is NOT a frontier planet then ignore
-        if ( p->Owner() == ENEMY && ! ( targets_1[p_id] || targets_2[p_id] ) ) {
-            continue;
-        }
-
-        // Don't try to neutral steal planets that we would not otherwise attack
-        if ( future_owner == ENEMY && p->Owner() == NEUTRAL && ! targets_2[p_id] ) {
-            continue;
-        }
-
-        int score = ScorePlanet(pw,p, defence_exclusions);
-        scores.push_back( std::pair<int,int>(score, p_id) );
-    }
-
-    // sort scores
-    std::sort(scores.begin(), scores.end());
-
-    LOG(" Starting attacks");
-
-    // start attacking planets based on score
-    for ( int i=0; i < scores.size(); ++i) {
-        std::pair<int,int> s = scores[i];
-        int p_id = s.second;
-        PlanetPtr p = pw.GetPlanet(p_id);
-
-        // list of the orders that we will want to execute
-        std::vector<Fleet> orders;
-
-        ScorePlanet(pw, p, defence_exclusions, orders);
-
-        if ( orders.empty() ) {
-            // This case happens when cost > available_ships
-
-            // If we don't have enough ships to capture the next best planet then WAIT
-            //  This serves a few purposes:
-            //  1. Prevent us overextending our forces
-            //  2. If we wait we might get the required resources later
-            //  3. This helps against Rage tactics
-            // Harass(pw, p_id, orders);
-            break;
-        }
-
-        // update delays so all fleet arrive at once
-        // TODO: Try leaving this out (Might harm neutral attack/overtakes)
-        const Fleet& last_order = orders.back();
-        int last_arrival = last_order.launch + Map::Distance( last_order.source, last_order.dest );
-        for ( int j=0; j<orders.size()-1; ++j ) {
-            Fleet& order = orders[j];
-            order.launch = last_arrival - Map::Distance( order.source, order.dest );
-        }
-
-        // If we reached here we want to actually execute the orders
-        for ( int j=0; j<orders.size(); ++j ) {
-            pw.IssueOrder(orders[j]);
-        }
-        
-    }
-}
 
 
 bool SortByGrowthRate(PlanetPtr a, PlanetPtr b) {
