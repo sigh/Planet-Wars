38106c3e314b721de1eebb3b59458b75484430d9
sigh <sigh.projects@gmail.com>
2010-11-02 19:19:55 +1100

    Put Flee code back in

--- src/DoTurn.cc
+++ src/DoTurn.cc
@@ -9,6 +9,8 @@
 #include "strategy/defence.h"
 #include "strategy/redist.h"
 
+void Flee(GameState &state, Player player);
+
 void DoTurn(const GameState& initial_state, std::vector<Fleet>& orders, Player player /* = ME */) {
     int my_planet_count = initial_state.PlanetsOwnedBy(player).size();
     if ( my_planet_count == 0 ) {
@@ -25,6 +27,8 @@ void DoTurn(const GameState& initial_state, std::vector<Fleet>& orders, Player p
 
     Attack(state, defence_exclusions, player);
 
+    Flee(state, player);
+
     Redistribution(state);
 
     LOG("Finishing up");
@@ -40,64 +44,66 @@ void DoTurn(const GameState& initial_state, std::vector<Fleet>& orders, Player p
 #include "strategy/attack.cc"
 #include "strategy/redist.cc"
 
-/*
- 
-
-void Flee(PlanetWars& pw);
-void Harass(PlanetWars& pw, int planet, std::vector<Fleet>& orders);
-
 bool SortByGrowthRate(PlanetPtr a, PlanetPtr b) {
-    return Map::GrowthRate(a->PlanetID()) < Map::GrowthRate(b->PlanetID()); 
+    return a->GrowthRate() < b->GrowthRate();
 }
 
-void Flee(PlanetWars& pw) {
+void Flee(GameState &state, Player player) {
     static bool flee = Config::Value<bool>("flee");
     if ( ! flee  ) return;
 
     LOG("Flee phase");
 
-    std::vector<PlanetPtr> my_planets = pw.PlanetsOwnedBy(ME);
+    std::vector<PlanetPtr> my_planets = state.PlanetsOwnedBy(player);
     sort(my_planets.begin(), my_planets.end(), SortByGrowthRate);
 
-    std::vector<PlanetPtr> planets = pw.Planets();
+    std::vector<PlanetPtr> planets = state.Planets();
 
-    for ( int i=0; i<my_planets.size(); ++i ) {
-        PlanetPtr p = my_planets[i]; 
-        if ( p->FutureOwner() == ME ) continue;
+    foreach ( PlanetPtr &p, my_planets ) {
+        if ( p->FutureOwner() == player ) continue;
 
-        int p_id = p->PlanetID();
+        int p_id = p->id;
         int ships = p->FutureState(0).ships;
 
         int closest_distance = INF;
         int destination = -1;
 
         // if the future owner is not me then see if we can use our ships elsewhere
-        for ( int j=0; j<planets.size(); ++j ) {
-            if ( j == p_id ) continue; 
+        foreach ( PlanetPtr &dest, planets ) {
+            int dest_id = dest->id;
+            if ( dest_id == p_id ) continue; 
+            if ( p->GrowthRate() >= dest->GrowthRate() ) break;
 
-            PlanetPtr dest = planets[j];
             if ( dest->Owner() == NEUTRAL ) continue;
-            if ( dest->FutureOwner() != ENEMY ) continue;
+            if ( dest->FutureOwner() != -player ) continue;
 
-            int distance = Map::Distance(p_id, j);
-            int cost = dest->Cost(distance);
+            int distance = Map::Distance(p_id, dest_id);
+            int cost = dest->Cost(distance, player);
 
-            LOG( "   Cost from " << p_id << " to " << j << ": " << cost );
+            LOG( "   Cost from " << p_id << " to " << dest_id << ": " << cost );
 
             if ( cost > ships ) continue;
 
             if ( distance < closest_distance ) {
                 closest_distance = distance;
-                destination = j;
+                destination = dest_id;
             }
         }
 
         if ( destination >= 0 ) {
-            pw.IssueOrder(Fleet(p_id, destination, ships));
+            state.IssueOrder(Fleet(player, p_id, destination, ships));
             LOG( " Fleeing from " << p_id << " to " << destination );
         }
     }
 }
+
+/*
+ 
+
+void Flee(PlanetWars& pw);
+void Harass(PlanetWars& pw, int planet, std::vector<Fleet>& orders);
+
+
 
 
 
